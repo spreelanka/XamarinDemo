d3e5ee87b80a900f55ca9ec1e212e3f030e2d68d
spreelanka <neil.j.hudson@gmail.com>
2016-02-10 15:06:50 -0800

    added servicestack template apiserver

---
+++ DemoDemo/DemoDemo.ApiServer/DemoDemo.ApiServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoDemo.ApiServer.Global" Language="C#" %>
---
+++ DemoDemo/DemoDemo.ApiServer/DemoDemo.ApiServer/Global.asax.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoDemo.ApiServer
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new AppHost().Init();
+        }
+    }
+}
